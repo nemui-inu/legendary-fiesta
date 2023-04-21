974997c1af9e6e90f7a62e89fc4fec32fd5c5dda
Nemui_Acer <jethrogarciafifth@gmail.com>
2023-04-21 20:53:00 +0800

    completed f_w_c

---
+++ blue/fun_with_commas.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class commas{
+public:
+  void print_with_commas(std::vector<std::string> p_input){
+    std::cout << std::endl << "You entered: ";
+    for(int i = 0; i < p_input.size(); i++){
+      if((i + 1) == p_input.size()){
+        std::cout << "and " << p_input.at(i) << "." << std::endl;
+      } else{
+        std::cout << p_input.at(i) << ", ";
+      }
+    }
+  }
+};
+
+int main(){
+  std::vector<std::string> input;
+
+  for(int i = 0; i < 50; i++){
+    std::string t;
+    std::cout << "Enter next word or \"quit\": ";
+    std::cin >> t;
+
+    if(t.compare("quit")){
+      input.push_back(t);
+    } else {
+      break;
+    } 
+  }
+
+  commas output;
+  output.print_with_commas(input);
+
+  return 0;
+}
